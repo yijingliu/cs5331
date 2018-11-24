f5019b8d051d6467f7d20fd5845eb1ae60f056fd
Liu, Yijing | RASIA <yijing.liu@rakuten.com>
2018-11-24 17:50:32 +0900

    Added UI

--- blocker.js
+++ blocker.js
@@ -1,14 +1,30 @@
 "use strict";
 
-$(document).on("change", "#track-request", function() {
-  var message = {track: this.checked}; 
+$(document).on("click", ".blocker-title", function() {
+  $(this).next(".blocker-details").toggle();
+});
 
-  // one-time communication
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
-      console.log(response.farewell);
-    });
+$(document).on("change", "input", function() {
+  var message = {}; 
+  message["advertising"] = $("#advertising").prop('checked');
+  message["site-analytics"] = $("#site-analytics").prop('checked');
+  message["third-party-cookie"] = $("#third-party-cookie").prop('checked');
+  message["browser-fingerprint"] = {
+      "user-agent": $("#user-agent").prop('checked'),
+      "content-language": $("#content-language").prop('checked'), 
+      "timestamp": $("#timestamp").prop('checked')
+  }
+
+  chrome.runtime.sendMessage(message, function (response) {
+  	console.log(response);
   });
 
-  
 });
+
+$(document).on("click", "#browser-fingerprint", function() {
+  var checked = $(this).prop('checked');
+  $('#user-agent').prop('checked', checked);
+  $('#content-language').prop('checked', checked);
+  $('#timestamp').prop('checked', checked);
+});
+
