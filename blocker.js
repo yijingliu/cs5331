c26fbfb79a4def8226aef216b054debd03e898e0
Liu, Yijing | RASIA <yijing.liu@rakuten.com>
2018-11-24 19:46:38 +0900

    resolve conflicts;

--- blocker.js
+++ blocker.js
@@ -5,33 +5,61 @@ $(document).ready(function() {
     console.log("Init from background");
     console.log(response);
 
-  	$("#advertising").prop('checked', response["user_selections"]["advertising"]);
-  	$("#site-analytics").prop('checked', response["user_selections"]["site-analytics"]);
-  	$("#third-party-cookie").prop('checked', response["user_selections"]["third-party-cookie"]);
-  	$("#user-agent").prop('checked', response["user_selections"]["browser-fingerprint"]["user-agent"]);
-  	$("#content-language").prop('checked', response["user_selections"]["browser-fingerprint"]["content-language"]);
-  	$("#timestamp").prop('checked', response["user_selections"]["browser-fingerprint"]["timestamp"]);
-  	if (response["user_selections"]["browser-fingerprint"]["user-agent"] 
+    $("#advertising").prop("checked", response["user_selections"]["advertising"]);
+    $("#site-analytics").prop("checked", response["user_selections"]["site-analytics"]);
+    $("#third-party-cookie").prop("checked", response["user_selections"]["third-party-cookie"]);
+    $("#user-agent").prop("checked", response["user_selections"]["browser-fingerprint"]["user-agent"]);
+    $("#content-language").prop("checked", response["user_selections"]["browser-fingerprint"]["content-language"]);
+    $("#timestamp").prop("checked", response["user_selections"]["browser-fingerprint"]["timestamp"]);
+    if (response["user_selections"]["browser-fingerprint"]["user-agent"] 
       && response["user_selections"]["browser-fingerprint"]["content-language"] 
       && response["user_selections"]["browser-fingerprint"]["timestamp"]) {
-  	  $("#browser-fingerprint").prop('checked', true);
-  	}
+      $("#browser-fingerprint").prop("checked", true);
+    }
   });
+
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+  	var tabId = tabs[0].id; 
+  	if (tabId != null && tabId != undefined)
+  	chrome.runtime.sendMessage({"request_type": "stats", "tab_id": tabId, }, function (response) {
+  	  response = {};
+  	  if (response["advertising"]) {
+  	    $.each(response["advertising"], function(key, value) {
+  	      console.log(key, value);
+  	    });
+  	  }
+    });
+  });
+
 });
+
+$(document).on("mouseover", ".blocker-details-record", function() {
+  $(this).next(".blocker-details-tooltip").prop("visibility", "visible");
+});
+
+
+
 
 $(document).on("click", ".blocker-title", function() {
   $(this).next(".blocker-details").toggle();
 });
 
+$(document).on("click", "#browser-fingerprint", function() {
+  var checked = $(this).prop("checked");
+  $("#user-agent").prop("checked", checked);
+  $("#content-language").prop("checked", checked);
+  $("#timestamp").prop("checked", checked);
+});
+
 $(document).on("change", "input", function() {
   var message = {}; 
-  message["advertising"] = $("#advertising").prop('checked');
-  message["site-analytics"] = $("#site-analytics").prop('checked');
-  message["third-party-cookie"] = $("#third-party-cookie").prop('checked');
+  message["advertising"] = $("#advertising").prop("checked");
+  message["site-analytics"] = $("#site-analytics").prop("checked");
+  message["third-party-cookie"] = $("#third-party-cookie").prop("checked");
   message["browser-fingerprint"] = {
-      "user-agent": $("#user-agent").prop('checked'),
-      "content-language": $("#content-language").prop('checked'), 
-      "timestamp": $("#timestamp").prop('checked')
+      "user-agent": $("#user-agent").prop("checked"),
+      "content-language": $("#content-language").prop("checked"), 
+      "timestamp": $("#timestamp").prop("checked")
   }
 
   chrome.runtime.sendMessage(message, function (response) {
@@ -45,10 +73,4 @@ $(document).on("change", "input", function() {
     });
 });
 
-$(document).on("click", "#browser-fingerprint", function() {
-  var checked = $(this).prop('checked');
-  $('#user-agent').prop('checked', checked);
-  $('#content-language').prop('checked', checked);
-  $('#timestamp').prop('checked', checked);
-});
 
--- track.js
+++ track.js
@@ -1,9 +1,1 @@
 "use strict";
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender);
-    console.log(request.track);
-    sendResponse({farewell: "bye"});
-  }
-);
