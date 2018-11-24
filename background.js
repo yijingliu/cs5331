f028a5c97cc752a02a47ac2d5a61b05308da9ce4
yuanda <superyouyun@gmail.com>
2018-11-24 15:05:39 +0800

    remove user agent and language, update timestamp

--- background.js
+++ background.js
@@ -1,5 +1,17 @@
 "use strict";
 
+// Object.defineProperty(navigator, 'userAgent', {
+//     get: function () { return 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0)'; }
+// });
+
+
+navigator.__defineGetter__('userAgent', function () {
+    return "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0)";
+});
+navigator.__defineGetter__('appName', function () {
+    return "Netscape";
+});
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
@@ -29,36 +41,71 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
   }
 
   // remove third party cookie
-  if (details.tabId > -1) {
-    chrome.tabs.get(details.tabId, function (tab) {
-      var initiator = extractHostname(tab.url);
-      var target = extractHostname(details.url);
-
-      var fromDomain = psl.parse(initiator).domain.split(".")[0];
-      var toDomain = psl.parse(target).domain.split(".")[0];
-      console.log("### fromDomain: ");
-      console.log(fromDomain);
-      console.log("### toDomain: ");
-      console.log(toDomain);
-
-      if (!sameRootDomain(initiator, target)) {
-        console.log("remove third party cookie : " + initiator + " -> " + target);
-        for (var i = 0; i < details.requestHeaders.length; ++i) {
-          if (details.requestHeaders[i].name === "Cookie") {
-            details.requestHeaders.splice(i, 1);
-            break;
-          }
+  if (details.hasOwnProperty("tabId") && details.tabId > -1) {
+    try {
+      chrome.tabs.get(details.tabId, function (tab) {
+        var tab_url = "www.cs5331_1.com";
+        if (tab === null || tab === undefined) {
+          console.log("tab is undefined");
+        } else {
+          tab_url = tab.url;
         }
-      }
-    });
+
+        var initiator = extractHostname(tab_url);
+        var target = extractHostname(details.url);
+
+        var fromDomain = psl.parse(initiator).domain;
+        if (fromDomain === null) {
+          fromDomain === "";
+        } else {
+          fromDomain = fromDomain.split(".")[0];
+        }
+        var toDomain = psl.parse(target).domain.split(".")[0];
+        console.log("### fromDomain: ");
+        console.log(fromDomain);
+        console.log("### toDomain: ");
+        console.log(toDomain);
+
+        if (!sameRootDomain(initiator, target)) {
+          console.log("remove third party cookie : " + initiator + " -> " + target);
+
+          var idxToRemove = [];
+          for (var i = 0; i < details.requestHeaders.length;) {
+            if (details.requestHeaders[i].name === "Cookie") {
+              console.log("removing header cookie")
+              details.requestHeaders.splice(i, 1);
+              continue;
+            }
+            if (details.requestHeaders[i].name === "User-Agent") {
+              console.log("removing header user agent");
+              details.requestHeaders.splice(i, 1);
+              continue;
+            }
+            if (details.requestHeaders[i].name === "Accept-Language") {
+              console.log("removing header accept language");
+              details.requestHeaders.splice(i, 1);
+              continue;
+            }
+            i++;
+          }
+
+          details.timeStamp = Math.round((new Date("1998-11-11T00:00:00")).getTime() / 1000);
+        }
+      })
+    } catch (e) {
+      console.log("Tab does not exists");
+      console.log(e);
+    }
   } 
 
   // block request without origin
-  if (details.tabId < 0) {
-    console.log("block non-origin request : " + target);
-    return {cancel: true};
-  }
+  // if (details.tabId < 0) {
+  //   console.log("block non-origin request : " + target);
+  //   return {cancel: true};
+  // }
 
+  console.log("Updated details: ");
+  console.log(details);
 
   return {requestHeaders: details.requestHeaders};
 }, {urls: ["<all_urls>"]}, ["blocking", "requestHeaders"]);
